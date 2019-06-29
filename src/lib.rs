732c145a72856485056e397b39ce396ab86e2a03
Mark McCaskey <maoeurk@gmail.com>
2019-06-29 12:15:03 -0700

    update deps and manifest for release of 0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-//! Mostly based on info from http://marc.rawer.de/Gameboy/Docs/GBCPUman.pdf (An amazing GameBoy resource with very few mistakes)
+//! A parser for Gameboy ROMS. Provides high-level useful data types like `RomHeader`
+//! and `RomType`. Basic validation is provided through the `validate` method
+//! on `RomHeader`.
+//!
+//! NOTE: this crate does nothing with the data before or after the ROM header right now.
+//! The bytes there are not validated as valid Gameboy machine code.
+//!
+//! Mostly based on info from http://marc.rawer.de/Gameboy/Docs/GBCPUman.pdf .
+//! Information from other places is and other places is called out in comments in the relevant files
 
 pub mod data;
 pub mod parser;
