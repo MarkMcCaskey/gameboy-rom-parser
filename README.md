64d40d842ac98ee1933dbddcfa625e57ede07571
Mark McCaskey <mark@wasmer.io>
2019-12-23 13:17:28 -0500

    Update the gbstats example program to be more interesting

--- README.md
+++ README.md
@@ -3,14 +3,13 @@
 [![Build Status](https://travis-ci.org/MarkMcCaskey/gameboy-rom-parser.svg?branch=master)](https://travis-ci.org/MarkMcCaskey/gameboy-rom-parser)
 [![Crates.io Version](https://img.shields.io/crates/v/gameboy-rom.svg)](https://crates.io/crates/gameboy-rom)
 
-A very simple parser to get data out of GB ROMs and perform basic validation.
-
-It's a design goal to make validation generally optional.
+A parser to get data out of GB ROMs and perform basic validation.  It provides a streaming opcode parser as well as high-level types for inspecting the Gameboy ROM's header.
 
 ## Demonstration
 
 ```shell
 cargo run --bin gb2json --features="serde_json" -- /path/to/rom/data
+cargo run --bin gbstats -- /path/to/rom/data
 ```
 
 And [here](https://github.com/MarkMcCaskey/rusty-boy/blob/master/src/cpu/cartridge/mod.rs)'s it in use in a real emulator ([rusty-boy]). 
--- src/bin/gbstats.rs
+++ src/bin/gbstats.rs
@@ -1,6 +1,13 @@
+//! A hacky little program to count the frequency of Gameboy instructions.
+//! Please note that this doesn't handle relative jumps, has no awareness of ROM
+//! banks, and double counts instructions if they were arrived at through a
+//! different entry-point.
+//!
+//! This program is for API demonstration purposes only.
 use std::io::Read;
 
 use gameboy_rom::{GameBoyRom, opcodes::Opcode};
+use std::collections::*;
 
 fn main() {
     let mut args = std::env::args();
@@ -16,9 +23,46 @@ fn main() {
     file.read_to_end(&mut bytes).expect("read bytes from file");
 
     let gbr = GameBoyRom::new(bytes.as_slice());
-    let intro_section = gbr.get_instructions_at(0x100);
+    // seen jump locations
+    let mut seen_locations: HashSet<usize> = HashSet::new();
+    // locations we want to analyze
+    let mut to_inspect: VecDeque<usize> = VecDeque::new();
+    // instructions we've seen
+    let mut seen_instructions: HashMap<Opcode, usize> = HashMap::new();
 
-    for o in intro_section {
-        dbg!(o);
+    // init start location
+    seen_locations.insert(0x100);
+    to_inspect.push_back(0x100);
+
+    while let Some(loc) = to_inspect.pop_front() {
+        for o in gbr.get_instructions_at(loc) {
+            match o {
+                Opcode::Call(_, address) | Opcode::Jp(_, address) =>  {
+                    let adj_adr = address as usize;
+                    if !seen_locations.contains(&adj_adr) {
+                        seen_locations.insert(address as _);
+                        to_inspect.push_back(address as _);
+                    }
+                }
+                _ => (),
+            }
+            let ent = seen_instructions.entry(o.clone()).or_default();
+            *ent += 1;
+        }
+    }
+
+    let mut vec = seen_instructions.into_iter().collect::<Vec<(Opcode, usize)>>();
+    vec.sort_by(|(_, a), (_, b)| b.cmp(&a));
+
+    println!("The top 10 most common instructions were:");
+    for i in 0..10 {
+        println!("{:>2}. {:?} appearing {} times", (i + 1), vec[i].0, vec[i].1);
+    }
+
+    println!("");
+    println!("The top 10 least common instructions were:");
+    for i in 0..10 {
+        let elem = vec[vec.len() - i - 1];
+        println!("{:>2}. {:?} appearing {} times", (i + 1), elem.0, elem.1);
     }
 }
